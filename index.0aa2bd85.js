58bcf72d0045f2f8ee3fa5e8285ea93d3cd193c6
Jasper <jasperfernando.b@gmail.com>
2023-03-20 22:25:59 +0530

    add

---
+++ index.0aa2bd85.js
@@ -0,0 +1,2 @@
+var e,t,n,c;jQuery,e=void 0,t=void 0,n=function(){t.addEventListener("click",(function(){return c(e,"nav-active")}))},c=function(e,t){e.classList.contains(t)?e.classList.remove(t):e.classList.add(t)},e=document.querySelector("body"),t=document.querySelector(".menu-icon"),document.querySelectorAll(".nav__list-item"),n();
+//# sourceMappingURL=index.0aa2bd85.js.map
